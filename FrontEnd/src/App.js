776f1f30e7aba534c07279802fcbac86685bfba8
hamdyAbdelazeim <hamdyabdelazeim@gmail.com>
2022-11-29 04:26:09 +0200

    strarting the project

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>hello from here </h1>
+        <h1>no more waning just do it </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
